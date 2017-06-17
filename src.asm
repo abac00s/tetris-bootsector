518c0c5c52e6d7ec6ffee20f10983c1c252fba06
Adam Bac <abac00s@users.noreply.github.com>
2017-06-17 20:37:11 +0200

    Added licence header

--- src.asm
+++ src.asm
@@ -1,10 +1,27 @@
-; TETRIS by abac00s
-; 
+; Simple Tetris (not fully functional) running as a 512B bootloader.
+; Copyright (C) 2017  Adam Bac
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+; Usage:
+;
 ; Use left and right arrows to move the falling brick.
 ; When you complete one row, it is removed, other
 ; bricks fall on its place and your score
-; (displayed in left-top corner) is increased.
-; The game is over, when bricks reach the top of the screen.
+; (displayed in the top-left corner) is increased.
+; The game is over when bricks reach the top of the screen.
 
 
 [bits 16]
